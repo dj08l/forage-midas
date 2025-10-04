29934d6cfb0de65b15462612ba99bac6013cbb75
--global <--global>
2025-10-04 11:03:06 +0530

    Initial commit

---
+++ src/main/java/com/jpmc/midascore/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jpmc.midascore.repository;
+
+import com.jpmc.midascore.entity.UserRecord;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserRecord, Long> {
+    UserRecord findById(long id);
+    UserRecord findByName(String name);
+}
